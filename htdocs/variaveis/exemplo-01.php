bf4ccc802969ad3686b5192138714c4a28dad3d5
Thyago Nunes <33403633+DarthVeigan@users.noreply.github.com>
2018-12-02 04:02:29 -0200

    Add files via upload
    Dando continuidade nas inutilidades que posso fazer com essa linguagem!

---
+++ htdocs/variaveis/exemplo-01.php
@@ -0,0 +1,7 @@
+<?php
+
+$nome = "Thyago";
+
+echo($nome);
+
+?>
---
+++ htdocs/variaveis/exemplo-02.php
@@ -0,0 +1,12 @@
+<?php
+
+$anoNascimento = 1993;
+$nomeCompleto = "Thyago Oliveira Nunes";
+
+echo("Seu nome completo é: $nomeCompleto<br>");
+echo("Seu ano de nascimento é: $anoNascimento");
+
+/*var_dump($anoNascimento);
+var_dump($nomeCompleto);*/
+
+?>
